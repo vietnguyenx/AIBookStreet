1094e0416bc0c3ca262dc24a7d45e3979b0eef95
it-is-feasible <hiepmap1579@gmail.com>
2025-01-12 16:30:19 +0700

    implement bookAuthor methods

--- AIBookStreet.Services/Services/Service/AuthorService.cs
+++ AIBookStreet.Services/Services/Service/AuthorService.cs
@@ -54,7 +54,20 @@ namespace AIBookStreet.Services.Services.Service
             if (existAuthor == null)
             {
                 return (1, null); //author khong ton tai
-            }
+            }            
+            existAuthor = await SetBaseEntityToUpdateFunc(existAuthor);
+
+            //var bookAuthors = await _repository.BookAuthorRepository.GetByElement(null, id);
+            //if (bookAuthors != null)
+            //{
+            //    foreach (var bookAuthor in bookAuthors)
+            //    {
+            //        bookAuthor.LastUpdatedBy = existAuthor.LastUpdatedBy;
+            //        bookAuthor.LastUpdatedDate = DateTime.Now;
+            //        bookAuthor.IsDeleted = true;
+            //    }
+            //}
+
             return await _repository.AuthorRepository.Delete(existAuthor) ? (2, existAuthor) //delete thanh cong
                                                                           : (3, null);       //delete fail
         }
