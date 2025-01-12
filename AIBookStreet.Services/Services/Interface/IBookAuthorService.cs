4fa5c97a50598fbb0408751a78ad0838a500859d
it-is-feasible <hiepmap1579@gmail.com>
2025-01-13 01:22:12 +0700

    implement bookCategory methods

--- AIBookStreet.Services/Services/Interface/IBookAuthorService.cs
+++ AIBookStreet.Services/Services/Interface/IBookAuthorService.cs
@@ -14,7 +14,7 @@ namespace AIBookStreet.Services.Services.Interface
         Task<(long, BookAuthor?)> UpdateABookAuthor(Guid bookAuthorID, BookAuthorModel bookAuthorModel);
         Task<(long, BookAuthor?)> DeleteABookAuthor(Guid id);
         Task<BookAuthor?> GetABookAuthorById(Guid id);
-        Task<List<BookAuthor>?> GetABookAuthorByElement(Guid? bookID, Guid? authorID);
+        Task<List<BookAuthor>?> GetBookAuthorByElement(Guid? bookID, Guid? authorID);
         Task<List<BookAuthor>?> GetAllActiveBookAuthors();
         Task<(List<BookAuthor>?, long)> GetAllBookAuthorsPagination(string? key, Guid? bookID, Guid? authorID, int? pageNumber, int? pageSize, string? sortField, bool? desc);
     }
