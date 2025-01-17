1de672f0c752cd29ada3aebbee07f80bfbf073cf
it-is-feasible <hiepmap1579@gmail.com>
2025-01-17 23:45:33 +0700

    something to deploy

--- AIBookStreet.API/Program.cs
+++ AIBookStreet.API/Program.cs
@@ -122,12 +122,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
