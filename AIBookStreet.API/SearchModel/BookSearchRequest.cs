c1f5e33df285e02f4dd95f60a56e06c6640cfa31
Nguyen Xuan Viet <vietnguyenxuan3112@gmail.com>
2025-01-27 05:50:47 +0700

    feat: enhance PublicationDate filtering in search

--- AIBookStreet.API/SearchModel/BookSearchRequest.cs
+++ AIBookStreet.API/SearchModel/BookSearchRequest.cs
@@ -4,10 +4,12 @@
     {
         public string? Code { get; set; }
         public string? Title { get; set; }
-        public DateTime? PublicationDate { get; set; }
         public decimal? Price { get; set; }
         public string? Languages { get; set; }
         public string? Size { get; set; }
         public string? Status { get; set; }
+        public DateTime? StartDate { get; set; } 
+        public DateTime? EndDate { get; set; } 
     }
+
 }
