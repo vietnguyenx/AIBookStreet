65792beb9eb7b543187af5034a4c10eb8698ef13
it-is-feasible <hiepmap1579@gmail.com>
2025-04-09 19:17:11 +0700

    update evt db

--- AIBookStreet.Repositories/Migrations/20250409121427_event_ads.Designer.cs
+++ AIBookStreet.Repositories/Migrations/20250409121427_event_ads.Designer.cs
@@ -4,6 +4,7 @@ using AIBookStreet.Repositories.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AIBookStreet.Repositories.Migrations
 {
     [DbContext(typeof(BSDbContext))]
-    partial class BSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250409121427_event_ads")]
+    partial class event_ads
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
