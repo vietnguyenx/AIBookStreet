08013caaec47d244d303aa8a0b75b67e1ed9b8df
it-is-feasible <hiepmap1579@gmail.com>
2025-04-25 09:18:06 +0700

    handle order status and payment method

---
+++ AIBookStreet.Services/Common/OrderConstant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIBookStreet.Services.Common
+{
+    public static class OrderConstant
+    {
+        public const string ORDER_PROGRESS = "InProgress";
+        public const string ORDER_COMPLETED = "Completed";
+        public const string ORDER_CANCELLED = "Cancelled";
+    }
+}
---
+++ AIBookStreet.Services/Common/PaymentMethodConstant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIBookStreet.Services.Common
+{
+    public static class PaymentMethodConstant
+    {
+        public const string CASH = "Cash";
+        public const string TRANSFER = "Transfer";
+    }
+}
